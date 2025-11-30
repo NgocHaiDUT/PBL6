c1f16a8bebe6bfd09aee290e41bfe380338ffb46
huyhoang <hoang01228590435@gmail.com>
2025-11-30 15:59:36 +0700

    Stop tracking prisma migrations

--- prisma/migrations/20251112075825_202511121457_update_message/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `message_type` on the `messages` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."messages" DROP COLUMN "message_type";
